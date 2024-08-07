f1ddeeb08ba4bbf46a08a774ac96aed27ecf33c7
PRIYA RANA <127029448+ranapiya@users.noreply.github.com>
2024-08-07 20:55:18 +0530

    Update page.tsx
    remove logo and add homePage instead

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -13,13 +13,15 @@ const AdminPage = async () => {
     <div className="mx-auto flex max-w-7xl flex-col space-y-14">
       <header className="admin-header">
         <Link href="/" className="cursor-pointer">
-          <Image
+
+          HomePage
+          {/* <Image
             src="/assets/icons/logo-full.svg"
             height={32}
             width={162}
             alt="logo"
             className="h-8 w-fit"
-          />
+          /> */}
         </Link>
 
         <p className="text-16-semibold">Admin Dashboard</p>
