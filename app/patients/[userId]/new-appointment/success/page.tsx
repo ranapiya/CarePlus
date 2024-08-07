7fb806f5b75fac6e47b2109a0c0c3754c5022c4c
PRIYA RANA <127029448+ranapiya@users.noreply.github.com>
2024-08-07 20:56:37 +0530

    Update page.tsx
    remove logo and  add home page

--- app/patients/[userId]/new-appointment/success/page.tsx
+++ app/patients/[userId]/new-appointment/success/page.tsx
@@ -21,13 +21,14 @@ const RequestSuccess = async ({
     <div className=" flex h-screen max-h-screen px-[5%]">
       <div className="success-img">
         <Link href="/">
-          <Image
+          HomePage
+          {/* <Image
             src="/assets/icons/logo-full.svg"
             height={1000}
             width={1000}
             alt="logo"
             className="h-10 w-fit"
-          />
+          /> */}
         </Link>
 
         <section className="flex flex-col items-center">
