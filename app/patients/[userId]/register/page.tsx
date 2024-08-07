3f8adf9b666a70cad68dc5fcd2af7e6f14109f9e
PRIYA RANA <127029448+ranapiya@users.noreply.github.com>
2024-08-07 20:53:27 +0530

    Update page.tsx
    removes image logo

--- app/patients/[userId]/register/page.tsx
+++ app/patients/[userId]/register/page.tsx
@@ -14,13 +14,13 @@ const Register = async ({ params: { userId } }: SearchParamProps) => {
     <div className="flex h-screen max-h-screen">
       <section className="remove-scrollbar container">
         <div className="sub-container max-w-[860px] flex-1 flex-col py-10">
-          <Image
+          {/* <Image
             src="/assets/icons/logo-full.svg"
             height={1000}
             width={1000}
             alt="patient"
             className="mb-12 h-10 w-fit"
-          />
+          /> */}
 
           <RegisterForm user={user} />
 
